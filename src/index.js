761243d5518e14b53b17245dc6eaeecef7977dd1
NgocTan2k1 <96438305+NgocTan2k1@users.noreply.github.com>
2022-11-22 11:44:54 +0700

    format folder and code

--- src/index.js
+++ src/index.js
@@ -1,28 +1,35 @@
-const express = require("express");
 const path = require('path');
+const express = require("express");
 const morgan = require("morgan");
 const handlebars = require("express-handlebars");
 const app = express();
 const port = 3000;
 
-app.use(express.static(path.join(__dirname, 'public'))) ;
+const route = require('./routes/index.js')
+
+
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.urlencoded({
+    extended: true
+}));
+app.use(express.json());
+
 //HTTP logger
 // app.use(morgan("combined"));
 app.engine('hbs', handlebars.engine({
     extname: ".hbs"
 }));
+
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'resource/views'));
 
-app.get('/', (req, res) => res.render('home'));
 
-app.get('/news', (req, res) => res.render('news'));
+// Home, Search, Contact
 
-app.get('/search', (req, res) => {
-    res.render('search');ẽit
-});
- 
 
+// Route init
+route(app);
 
 
 
